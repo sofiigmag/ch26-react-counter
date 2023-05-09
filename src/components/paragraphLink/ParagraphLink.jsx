f90eed3e54a0403f871e08131f352ca1866455bc
Sofia Gm <sofia.gmagk@gmail.com>
2023-05-09 14:31:56 -0600

    feat: componentes

---
+++ src/components/paragraphLink/ParagraphLink.jsx
@@ -0,0 +1,28 @@
+// Props: Comunican los componentes padres con los componentes hijos.
+
+const ParagraphLink = ( {href, color, children} ) => { // { clave:valor, href: ""}
+    //console.log ( props );
+
+    const myParagraph = (
+        <>
+            <a 
+                style = { { textDecoration: 'none', color: color } } 
+                href= {href}
+                target= 'blank'
+            >
+                <p> {children} </p>    
+            </a>
+        </>
+    );
+
+    return myParagraph;
+};
+
+export default ParagraphLink; // exportación por default.
+
+/*
+Desestructuración
+    
+    const [varA, , varB] = [23, 45. 67];
+    const {varC, varD: varG} = {varC, varF, varG,}
+*/
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import  P  from './components/paragraphLink/ParagraphLink'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <p href="http" > texto</p>*/}
+    <P href="https://legacy.reactjs.org/" pepito="Yo merengues" color ="green">Página React</P>
+    <P href="https:youtube.com" color="orange">Página Youtube</P>
+    <P href="https:linkedin.com" color= "pink">Página Linkedin</P>
+    <P href="https:linkedin.com/goyo" color="blueviolet">Página Goyo</P>
+    <P href="https:linkedin.com/goyo" color="blueviolet">Página Goyo</P>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
