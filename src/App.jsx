43e05d019c1058608af320c90674800e3dfbf635
Sofia Gm <sofia.gmagk@gmail.com>
2023-05-10 14:05:10 -0600

    ci: uso de props y hooks

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,23 @@ import { SocialNetworks } from "./components/socialNetworks/SocialNetworks";
 import { NavBar } from "./components/navBar/NavBar";
 import { Footer } from "./components/footer/Footer";
 import { Counter } from "./components/counter/Counter";
+import { Home } from "./components/home/Home";
+import { Route, Routes } from "react-router-dom";
+import { NotFound } from "./components/notfound/NotFound";
+
 
 export const App = () => {
   return (
     <>
       <NavBar />
-      <SocialNetworks />
-      <Counter initialValue={100} increment={5} decrement={5} /> 
-      <Counter initialValue={0} increment={10} decrement={5}/> 
+
+      <Routes>
+        <Route path="/" element={ <Home />} />
+        <Route path="social-networks" element={ <SocialNetworks />} />
+        <Route path="counter/1" element={<Counter initialValue={100} increment={5} decrement={5} /> } />
+        <Route path="counter/2" element={<Counter initialValue={0} increment={10} decrement={5} /> } />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Footer />
     </>
   );
---
+++ src/components/notfound/NotFound.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NotFound = () => {
+  return (
+    <h2 style={{ color: "pink" }}>Not Found</h2>
+  )
+}
